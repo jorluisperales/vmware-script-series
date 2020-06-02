5ed60b7a14077018fe47b39382d2ec42b9e5f877
jperales29 <jorluis.perales@gmail.com>
2020-06-02 17:08:11 -0500

    Update to version 1.1
    Added automatic VLAN assignment to newly PG based on node's mgmt vmk

--- net-recoveryvc.sh
+++ net-recoveryvc.sh
@@ -1,6 +1,7 @@
-# Script to obtain create a temporary vSS and PG to recovery network connectivity between vcsa and psc
+# Script to create a temporary vSS and PG to recovery network connectivity between vcsa and psc
 # Author: Jorluis Perales, VxRail SST @ Dell EMC
-# Version 1.0
+# Version 1.0 - Original version
+# Version 1.1 - Added automatic VLAN assignment to newly PG based on node's mgmt vmk
 #
 # Do not change anything below this line
 # --------------------------------------
@@ -12,13 +13,16 @@ esxcli network nic list
 echo ""
 vds=$(esxcli network vswitch dvs vmware list | grep -E "EVO:RAIL Distributed Switch|VMware HCIA Distributed Switch" | head -n 1)
 echo ""
-echo "Which vmnic you would like to disconect:"
+echo "Which vmnic you would like to disconnect:"
 
 read vmnic
+
 
 sleep 2
 
 portid=$(esxcli network vswitch dvs vmware list | grep "$vmnic" -A4 | grep "Port ID" | sed 's/[^0-9]*//g')
+platform=$(esxcli hardware platform get | grep "Vendor Name:" | awk '{print $3}')
+
 
 echo ""
 
@@ -29,7 +33,7 @@ esxcfg-vswitch -Q $vmnic -V $portid "$vds"
 
 echo ""
 sleep 1
-echo "$vmnic has been succefully removed, creating tmpsw switch"
+echo "$vmnic has been successfully removed, creating tmpsw switch"
 
 esxcli network vswitch standard add -v tmpsw
 
@@ -50,16 +54,18 @@ esxcli network vswitch standard portgroup add --portgroup-name=tmp --vswitch-nam
 
 echo ""
 
-echo "Would you like to add the vCenter/Management VLAN ID to the tmp port group? (Y/N)"
-
-read answer
-
-if [ $answer == y -o $answer == Y ];
+echo "The Script will automatically add the Management's vmk VLAN to the newly created Port Group"
+echo ""
+if [ $platform == Dell ];
 then
 
+echo "Hardware is $platform"
+echo ""
 
-    echo "Type the vCenter/Management VLAN ID:"
-	read vlan
+
+	vmk2dvp=$(esxcli network vswitch dvs vmware list | grep -i vmk2 -A1 | tail -n 1 | awk '{print $3}')
+	
+	vlan=$(net-dvs  | grep -i $vmk2dvp -A37 | egrep -i "com.vmware.common.port.volatile.vlan" | awk '{print $4}')
 
 sleep 1
 	
@@ -71,14 +77,22 @@ sleep 1
 echo ""	
 fi
 
-if [ $answer == n -o $answer == N ];
+if [ $platform == Quanta ];
 then
 
+echo "Hardware is $platform"
+echo ""
 
-echo "========="
-echo "|Alright|"
-echo "========="
+	vmk1dvp=$(esxcli network vswitch dvs vmware list | grep -i vmk1 -A1 | tail -n 1 | awk '{print $3}')
+	
+	vlan=$(net-dvs  | grep -i $vmk1dvp -A37 | egrep -i "com.vmware.common.port.volatile.vlan" | awk '{print $4}')
 
+sleep 1
+	
+echo "Setting VLAN ID $vlan to tmp switch"
+sleep 1
+
+esxcli network vswitch standard portgroup set --portgroup-name=tmp --vlan-id=$vlan 
 sleep 1
 
 echo ""
@@ -86,5 +100,5 @@ fi
 
 echo "All set, now go through the $(hostname)'s UI and add the new tmp port group to your vCenter/PSC"
 
-echo "=================================================================================================="
+echo "======================================================================================================="
 
